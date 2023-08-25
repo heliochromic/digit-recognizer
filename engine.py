c05b1bdffbec081ec4c8dd5754bc851e98642edb
bohdan <howchromium@gmail.com>
2023-08-25 15:20:24 +0300

    project upload

---
+++ engine.py
@@ -0,0 +1,77 @@
+import torch
+from torch.utils.data import DataLoader
+import random
+torch.manual_seed(42)
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+def train_step(model: torch.nn.Module,
+               data_loader: DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn,
+               device: torch.device):
+    train_loss, train_acc = 0, 0
+    model.to(device)
+    model.train()  # Set the model to training mode
+
+    for X, y in data_loader:
+        X, y = X.to(device), y.to(device)
+
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()  # Accumulate the loss value
+
+        train_acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))  # Accumulate accuracy
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
+
+def test_step(data_loader: torch.utils.data.DataLoader,
+              model: torch.nn.Module,
+              loss_fn: torch.nn.Module,
+              accuracy_fn,
+              device: torch.device):
+    test_loss, test_acc = 0, 0
+    model.to(device)
+    model.eval() # put model in eval mode
+    # Turn on inference context manager
+    with torch.inference_mode(): 
+        for X, y in data_loader:
+            # Send data to GPU
+            X, y = X.to(device), y.to(device)
+            
+            # 1. Forward pass
+            test_pred = model(X)
+            
+            # 2. Calculate loss and accuracy
+            test_loss += loss_fn(test_pred, y).item()
+            test_acc += accuracy_fn(y_true=y,
+                y_pred=test_pred.argmax(dim=1) # Go from logits -> pred labels
+            )
+        
+        # Adjust metrics and print out
+    test_loss /= len(data_loader)
+    test_acc /= len(data_loader)
+    print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
+
+
+def make_predictions(model: torch.nn.Module, data: list, device: torch.device):
+    pred_probs = []
+    model.eval()
+    with torch.inference_mode():
+        for sample in data:
+            sample = torch.unsqueeze(sample, dim=0).to(device)
+            pred_logit = model(sample)
+            pred_prob = torch.softmax(pred_logit.squeeze(), dim=0)
+            pred_probs.append(pred_prob.cpu())
+
+    return torch.stack(pred_probs)
+
